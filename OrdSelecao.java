52a11fbf7f814c3c9ac3a78e999af38d6cd557f9
Jhonatan-Gutemberg <mecjgr@hotmail.com>
2024-08-25 15:10:15 -0300

    Metodo de ordenação

---
+++ OrdSelecao.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class OrdSelecao {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String entrada;
+
+        do {
+            entrada = scanner.nextLine();
+            if (!entrada.equals("FIM")) {
+                int[] numeros = separaVetor(entrada);
+                ordenacaoSelecao(numeros);
+
+            }
+
+        } while (!entrada.equals("FIM") && scanner.hasNext());
+
+        scanner.close();
+    }
+
+    private static int[] separaVetor(String entrada) {
+        String[] partes = entrada.split(";");
+        int[] valores = new int[partes.length];
+
+        for (int i = 0; i < partes.length; i++) {
+            valores[i] = Integer.parseInt(partes[i]);
+        }
+
+        return valores;
+    }
+
+    private static void ordenacaoSelecao(int[] numeros) {
+        int tamanho = numeros.length;
+        int aux, min;
+
+        for (int i = 0; i < tamanho - 1; i++) {
+            min = i;
+
+            for (int j = (i + 1); j < tamanho; j++) {
+                if (numeros[j] < numeros[min])
+                    min = j;
+
+            }
+
+            if (min != i) {
+                aux = numeros[i];
+                numeros[i] = numeros[min];
+                numeros[min] = aux;
+            }
+
+        }
+
+        for (int num : numeros) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+O objetivo desta prática é ordenar vetores de inteiros aplicando o método de ordenação por seleção.
+
+Seu programa deve ler da entrada-padrão os conjuntos de números inteiros a serem ordenados.
+
+A entrada padrão é composta por várias linhas. Cada linha apresenta um conjunto de números inteiros, separados pelo símbolo “;”, a serem ordenados. Os valores inteiros podem ser positivos ou negativos. A entrada-padrão termina em uma linha com a palavra “FIM”.
+
+Ordene o conjunto de inteiros apresentado em cada linha da entrada-padrão.
+
+A saída padrão deve obedecer à seguinte regra: para cada linha da entrada padrão, uma linha com os valores ordenados, separados por um espaço em branco, e uma segunda linha com os dizeres “Comparacoes realizadas: ”, seguido do número de comparações realizadas pelo algoritmo naquele caso. Por exemplo:
+
+Entrada
+
+8;15;42;23;4;16
+
+2;10;-5;4;9
+
+FIM
+
+Saída Esperada
+
+4 8 15 16 23 42
+
+Comparacoes realizadas: 15
+
+-5 2 4 9 10
+
+Comparacoes realizadas: 10
